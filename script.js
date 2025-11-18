267e5548ebb74beb62f26120aae74a73b63c0425
pradooguilherme <guipradu2020@gmail.com>
2025-11-18 02:09:39 -0300

    feat: changes createGrid, removeGrid functions, creates getRandomInt, getNewGridSize functions

--- script.js
+++ script.js
@@ -1,25 +1,61 @@
-let numbRows = 10
-let numbColumns = 10
 
-function createGrid(numbRows, numbColumns, container) {
-    
-    for (let i = 0; i < numbRows; i++) {
-        
-        let lineContainer = document.createElement('div')
-        lineContainer.classList.add('line-container')
-        
-        for (let j = 0; j < numbColumns; j++) {
-            
-            let gridSquare = document.createElement('div')
-            lineContainer.appendChild(gridSquare)
+function createGrid(numb) {
 
-            gridSquare.addEventListener("mouseenter", () => {
-                gridSquare.style.opacity = Number(gridSquare.style.opacity) + 0.1
-            })
-        }
-        container.appendChild(lineContainer)
+    let cellDim = 960 / numb;
+    const gridContainer = document.querySelector('.flex-grid')
+
+    for (let i = 1; i <= numb * numb; i++) {
+
+        let gridElement = document.createElement('div');
+        let opacity = 1
+
+        gridElement.style.minWidth = `${cellDim}px`;
+        gridElement.style.backgroundColor = "white"
+
+        gridElement.addEventListener('mouseenter', () => {
+
+            if (!gridElement.style.background) {
+                gridElement.style.background = `rgb(${getRandomInt(256)},${getRandomInt(256)},${getRandomInt(256)})`
+            }
+
+            if (opacity > 0) {
+                opacity -= 0.1
+                gridElement.style.opacity = `${opacity}`
+            }
+        })
+        gridContainer.appendChild(gridElement)
     }
 }
 
+function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
+}
+
+function removeGrid(element) {
+    let child = divContainer.lastElementChild
+
+    while (child) {
+        element.removeChild(child)
+        child = element.lastElementChild
+    }
+}
+function getNewGridSize() {
+    let size = prompt('Insert the new size of the grid')
+
+    while (size > 100) {
+        size = prompt('Insert the new size of the grid:')
+    }
+
+    return size
+}
+
+let numb = 16
 const divContainer = document.querySelector('.flex-grid')
-createGrid(numbRows, numbColumns, divContainer)
+const button = document.querySelector('button')
+
+button.addEventListener('click', () => {
+    removeGrid(divContainer)
+    createGrid(getNewGridSize())
+})
+
+createGrid(numb)
